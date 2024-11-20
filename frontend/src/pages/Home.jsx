af7da53e8edee7533f3cbec0174fc9d0ae9c5371
Muh-Wale <muhammadolawale27@gmail.com>
2024-11-20 14:44:14 +0100

    Latest

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('https://book-web-backend.vercel.app/')
+      .get('https://book-web-backend.vercel.app/books/')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
